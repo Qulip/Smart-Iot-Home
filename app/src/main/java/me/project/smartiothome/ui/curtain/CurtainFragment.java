600fec6c6b36bbefde7517f5cae3b8a2bc045451
Qulip <77991314+Qulip@users.noreply.github.com>
2021-11-25 23:46:10 +0900

    Add Hander and delay on curtain buttons

--- app/src/main/java/me/project/smartiothome/ui/curtain/CurtainFragment.java
+++ app/src/main/java/me/project/smartiothome/ui/curtain/CurtainFragment.java
@@ -1,6 +1,7 @@
 package me.project.smartiothome.ui.curtain;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,13 +26,15 @@ public class CurtainFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_curtain, container, false);
         Button light_On = (Button) root.findViewById(R.id.light_On);
         Button light_Off = (Button) root.findViewById(R.id.light_Off);
+        Button curtain_Up = (Button) root.findViewById(R.id.curtain_up);
+        Button curtain_Down = (Button) root.findViewById(R.id.curtain_down);
         WebView webview = (WebView) root.findViewById(R.id.switchwebview);
         webview.setVisibility(View.INVISIBLE);
+        Handler handler = new Handler();
         light_On.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 webview.loadUrl(control_Url + "/led?pinD8=on");
-
             }
         });
         light_Off.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +43,30 @@ public class CurtainFragment extends Fragment {
                 webview.loadUrl(control_Url + "/led?pinD8=off");
             }
         });
-
+        curtain_Up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl(control_Url + "/blind?blind=up");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        webview.loadUrl(control_Url + "/blind?blind=stop");
+                    }
+                },2000);
+            }
+        });
+        curtain_Up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                webview.loadUrl(control_Url + "/blind?blind=down");
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        webview.loadUrl(control_Url + "/blind?blind=stop");
+                    }
+                },2000);
+            }
+        });
         return root;
     }
 }
